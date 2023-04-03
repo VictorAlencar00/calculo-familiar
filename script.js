e9372be1e6c97ab563526edae0b04c8eef6a32a8
VictorAlencar00 <128549998+VictorAlencar00@users.noreply.github.com>
2023-04-03 20:35:40 -0300

    Remoção de comments

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-"Versão chat gpt"
-
-// let saldoFamiliar = {
-//     incomes: [1300, 500, 300],
-//     expenses: [700, 850]
-// }
-// function rendaFamiliar(){
-//     let incomes = saldoFamiliar.incomes.reduce((total, income) => total + income)
-//     let expenses = saldoFamiliar.expenses.reduce((total, expense) => total + expense)
-
-//     if (incomes - expenses >= 0){
-//         console.log("Saldo positivo")
-//     } else {
-//         console.log("Saldo negativo")
-//     }
-//     return saldoFamiliar
-// }
-// rendaFamiliar()
-
 let familia = {
     entradas: [3200, 1700, 350],
     saidas: [2000, 900, 500]
@@ -27,8 +8,6 @@ function soma(array){
     for (let valor of array){
         total += valor
     }
-    //aqui é o FOR OF que faz com que a função saiba que é um array, o que não tem nada a ver com os nomes da variáveis que declarei
-
     return total
 }
 
